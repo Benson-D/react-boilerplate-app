ffda9cf1fbb5fae0b9433c567c7ea932131cb328
Dan Benson <dbenson3091@gmail.com>
2022-02-01 23:36:01 -0600

    created string list file

---
+++ app/components/StringList/tests/index.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { IntlProvider } from 'react-intl';
+import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router-dom';
+import { render } from 'react-testing-library';
+
+import StringList from '../index';
+import configureStore from '../../../configureStore';
+
+describe('<StringList />', () => {
+  it('should render the loading indicator when its loading', () => {
+    const { container } = render(<StringList loading />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render an error if loading failed', () => {
+    const { queryByText } = render(
+      <IntlProvider locale="en">
+        <StringList loading={false} error={{ message: 'Loading failed!' }} />
+      </IntlProvider>,
+    );
+    expect(queryByText(/Something went wrong/)).not.toBeNull();
+  });
+
+  it('should render the data if loading was successful', () => {
+    const store = configureStore(
+      { global: { currentUser: 'mxstbr' } },
+      browserHistory,
+    );
+    const testData = { strings: ['test1', 'test2'] };
+
+    const { container } = render(
+      <Provider store={store}>
+        <IntlProvider locale="en">
+          <StringList repos={testData} error={false} />
+        </IntlProvider>
+      </Provider>,
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should not render anything if nothing interesting is provided', () => {
+    const { container } = render(
+      <StringList repos={false} error={false} loading={false} />,
+    );
+
+    expect(container.firstChild).toBeNull();
+  });
+});
